3738fbd096e79172d1279b6cdd98e4a3646116db
Edwin Vasquez <evasquez@infile.com.gt>
2018-10-09 00:58:32 -0600

    Fin Seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
-    console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
 
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( ( resp: InfoPagina ) => {
+    .subscribe( ( resp: InfoPagina ) => {
 
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-        // console.log(resp['facebook']);
-      });
+      this.cargada = true;
+      this.info = resp;
+    });
+   }
+
+   private cargarEquipo() {
+
+    this.http.get('https://angular-html-5584c.firebaseio.com/equipo.json')
+    .subscribe( ( resp: any[] ) => {
+
+      this.equipo = resp;
+      console.log(resp);
+
+    });
    }
 }
