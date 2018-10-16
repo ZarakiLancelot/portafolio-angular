19fef435e6cc155f04596d860131517a7c3e929f
Edwin Vasquez <evasquez@infile.com.gt>
2018-10-16 13:01:34 -0600

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
 
@@ -18,18 +19,52 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://angular-html-5584c.firebaseio.com/productos_idx.json')
+    return new Promise( ( resolve, reject ) => {
+      this.http.get('https://angular-html-5584c.firebaseio.com/productos_idx.json')
         .subscribe( (resp: Producto[]) => {
 
           console.log(resp);
           this.productos = resp;
-          // this.cargando = false;
 
           setTimeout(() => {
             this.cargando = false;
+            resolve();
           }, 2000);
 
         });
+    });
+  }
 
+  getProducto( id: string) {
+    return this.http.get(`https://angular-html-5584c.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0) {
+      this.cargarProductos().then( () => {
+
+        this.filtrarProductos( termino );
+
+      });
+    } else {
+      this.filtrarProductos( termino );
+    }
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach ( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if ( prod.categoria.indexOf ( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+      }
+    });
   }
 }
